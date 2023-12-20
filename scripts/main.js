54fa57ab3ec95d73d3b90305a59745ba51b86ae4
hadil244 <hadil.ahmed@hva.nl>
2023-12-20 15:57:27 +0100

    mini interactie comment section
    (nog niet helemaal responsive)

--- scripts/main.js
+++ scripts/main.js
@@ -50,7 +50,6 @@ themecontrastToggleBtn.addEventListener('click', () => {
 });
 
 
- 
 let popup = document.getElementById("popup");
 
 function openPopup(){
@@ -60,3 +59,27 @@ function openPopup(){
 function closePopup(){
     popup.classList.remove("open-popup");
 }
+
+// Elementen selecteren; button, textarea, bericht en gehele operkingensectie
+
+let button = document.querySelector('.sndbtn')
+let commentInput = document.querySelector('textarea')
+let message = document.querySelector('.msg')
+let commentBox = document.querySelector('.comment-section')
+
+// Naar event luisteren (button wordt geklikt)
+
+button.addEventListener('click', function() {
+
+// Bericht verschijnt
+
+    message.classList.add('show-msg')
+})
+// Naar event luisteren (bericht wordt geklikt)
+
+message.addEventListener('click', function() {
+
+// bericht wordt verwijdert
+
+    message.remove('msg')
+})
